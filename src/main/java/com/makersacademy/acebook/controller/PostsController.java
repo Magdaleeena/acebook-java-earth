0877dc5f437964e4b6c198ef123e78d5460f8283
Chris Mecrow <mecrow@live.co.uk>
2025-07-22 11:32:22 +0100

    Added sorting by ID to post items

--- src/main/java/com/makersacademy/acebook/controller/PostsController.java
+++ src/main/java/com/makersacademy/acebook/controller/PostsController.java
@@ -3,6 +3,7 @@ package com.makersacademy.acebook.controller;
 import com.makersacademy.acebook.model.Post;
 import com.makersacademy.acebook.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +19,7 @@ public class PostsController {
 
     @GetMapping("/posts")
     public String index(Model model) {
-        Iterable<Post> posts = repository.findAll();
+        Iterable<Post> posts = repository.findAll(Sort.by(Sort.Direction.DESC, "id"));
         model.addAttribute("posts", posts);
         model.addAttribute("post", new Post());
         return "posts/index";
--- src/main/java/com/makersacademy/acebook/repository/PostRepository.java
+++ src/main/java/com/makersacademy/acebook/repository/PostRepository.java
@@ -2,8 +2,10 @@ package com.makersacademy.acebook.repository;
 
 import com.makersacademy.acebook.model.Post;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 
+    Iterable<Post> findAll(Sort id);
 }
