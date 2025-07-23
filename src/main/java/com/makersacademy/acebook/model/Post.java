1783e841a72f65fe948e0cb12d345ac869b20879
Magdalena Janeckova <magdalena.janeckova92@gmail.com>
2025-07-23 10:52:45 +0100

    Like functionality

--- src/main/java/com/makersacademy/acebook/model/Post.java
+++ src/main/java/com/makersacademy/acebook/model/Post.java
@@ -3,18 +3,19 @@ package com.makersacademy.acebook.model;
 import jakarta.persistence.*;
 
 import lombok.Data;
+import lombok.*;
 
 @Data
 @Entity
 @Table(name = "POSTS")
+@Getter @Setter @NoArgsConstructor
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String content;
-
-    public Post() {}
+    private int likeCount;
 
     public Post(String content) {
         this.content = content;
