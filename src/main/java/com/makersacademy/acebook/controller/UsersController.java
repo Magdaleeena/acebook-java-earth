09dc20f602b549d88fc9565de259a4a533c4e700
Chris Mecrow <mecrow@live.co.uk>
2025-07-24 17:07:21 +0100

    Added user profile page

--- src/main/java/com/makersacademy/acebook/controller/UsersController.java
+++ src/main/java/com/makersacademy/acebook/controller/UsersController.java
@@ -2,10 +2,13 @@ package com.makersacademy.acebook.controller;
 
 import com.makersacademy.acebook.model.User;
 import com.makersacademy.acebook.repository.UserRepository;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
@@ -13,18 +16,44 @@ public class UsersController {
     @Autowired
     UserRepository userRepository;
 
+    @ModelAttribute("user")
+    public User addLoggedInUserToModel(Authentication authentication) {
+        if (authentication != null && authentication.getPrincipal() instanceof DefaultOidcUser oidcUser) {
+            String email = (String) oidcUser.getAttributes().get("email");
+            return userRepository.findUserByUsername(email).orElse(null);
+        }
+        return null;
+    }
+
     @GetMapping("/users/after-login")
-    public RedirectView afterLogin() {
+    public RedirectView afterLogin(HttpSession session) {
         DefaultOidcUser principal = (DefaultOidcUser) SecurityContextHolder
                 .getContext()
                 .getAuthentication()
                 .getPrincipal();
 
         String username = (String) principal.getAttributes().get("email");
-        userRepository
+        StringBuilder displayNameCon = new StringBuilder();
+        if (username != null && username.contains("@")) {
+            for (int i = 0; i < username.length(); i++) {
+                char c = username.charAt(i);
+                if (c == '@') break;
+                displayNameCon.append(c);
+            }
+        }
+        String displayName = displayNameCon.toString();
+        User user = userRepository
                 .findUserByUsername(username)
-                .orElseGet(() -> userRepository.save(new User(username)));
-
+                .orElseGet(() -> userRepository.save(new User(username, displayName)));
+        session.setAttribute("user", user);
         return new RedirectView("/posts");
+    }
+
+    @GetMapping("/profile/{userId}")
+    public ModelAndView getUserbyId(@PathVariable("userId") Long userId) {
+        ModelAndView modelAndView = new ModelAndView("/profile/user");
+        User user = userRepository.findById(userId).orElse(null);
+        modelAndView.addObject("user", user);
+        return modelAndView;
     }
 }
